f7c46a56ce43bd3859b756f93ae3041c84641206
Igor Okulist <okigan@gmail.com>
2023-11-26 01:43:01 -0600

    up/down scaling function update and param processing

--- scale/scale.py
+++ scale/scale.py
@@ -10,7 +10,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 LAMBDA_FUNCTION_NAME = os.environ['LAMBDA_FUNCTION_NAME']
-STACK_NAME = 'MyFargateStack'
+STACK_NAME = os.environ['STACK_NAME']
 
 LAMBDA_CLIENT = boto3.client('lambda')
 CLOUDFORMATION_CLIENT = boto3.client('cloudformation')
@@ -28,50 +28,40 @@ def monitor(stack_name, target_status):
         time.sleep(5)
 
 
-
-def lambda_scale_down_handler(event, context):
-    CLOUDFORMATION_CLIENT.delete_stack(StackName=STACK_NAME)
-    monitor(STACK_NAME, 'DELETE_COMPLETE')
-    return {'message': 'Fargate stack deletion initiated'}
-
-
-
 def stack_exists(stack_name):
     try:
         CLOUDFORMATION_CLIENT.describe_stacks(StackName=stack_name)
         return True
     except ClientError as e:
         return 'does not exist' not in str(e)
-    
 
-def lambda_scale_up_handler(event, context):
-    subprocess.run(['ls', '-s'], check=True)
-
-    response = LAMBDA_CLIENT.get_function(FunctionName=LAMBDA_FUNCTION_NAME)
-    image_uri = response['Code']['ImageUri']
-    logger.info(f'Image URI: {image_uri}')
-
-    with open('template-fargate.yml', 'r') as template_file:
-        template_body = template_file.read()
-
-    create_or_update_stack = CLOUDFORMATION_CLIENT.create_stack if not stack_exists(STACK_NAME) else CLOUDFORMATION_CLIENT.update_stack
-
-    create_or_update_stack(
+def scale_up_down(paramWithFargate):
+    CLOUDFORMATION_CLIENT.update_stack(
         StackName=STACK_NAME,
-        TemplateBody=template_body,
+        UsePreviousTemplate=True,
         Capabilities=['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'],
-        # Include additional parameters if necessary, such as:
         Parameters=[
             {
-                'ParameterKey': 'ContainerImageUri',
-                'ParameterValue': image_uri
+                'ParameterKey': 'paramDomainName',
+                'UsePreviousValue': True
+            },
+            {
+                'ParameterKey': 'paramHostedZoneId',
+                'UsePreviousValue': True
+            },
+            {
+                'ParameterKey': 'paramWithFargate',
+                'ParameterValue': paramWithFargate
             }
         ],
     )
 
+
     monitor(STACK_NAME, 'CREATE_COMPLETE')
     return {'message': 'Fargate stack deployment initiated'}
 
+def lambda_scale_up_handler(event, context):
+    return scale_up_down(True)
 
-# arn:aws:lambda:us-east-1:602375570961:function:anyscale-dev-MyLambdaFunction-UU1H8oy6y9cs
-# arn:aws:lambda:us-east-1:602375570961:function:MyLambdaFunction
+def lambda_scale_down_handler(event, context):
+    return scale_up_down(False)
